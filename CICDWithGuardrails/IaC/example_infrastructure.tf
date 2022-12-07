fa902ab91b9af75403beedfb8291cda59b348cf8
stenio123 <stenio123+developer@gmail.com>
2022-12-07 14:55:02 -0600

    fixed terraform

--- CICDWithGuardrails/IaC/example_infrastructure.tf
+++ CICDWithGuardrails/IaC/example_infrastructure.tf
@@ -75,5 +75,6 @@ resource "google_storage_bucket" "default" {
 resource "google_storage_bucket_acl" "image-store-acl" {
   bucket = google_storage_bucket.default.name
 
-  predefined_acl = "publicRead"
+  predefined_acl = "private"
+  #predefined_acl = "publicRead"
 }
