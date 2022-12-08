4bb6510f464b5c26090ff25114d0c362cb422249
stenio123 <stenio123+developer@gmail.com>
2022-12-08 17:26:59 -0600

    Updated CICD bucket to allow destroy

--- CICDWithGuardrails/IaC_state_storage.tf
+++ CICDWithGuardrails/IaC_state_storage.tf
@@ -1,7 +1,8 @@
 ## This is state storage for IaC state only. It doesn't store state for the CICD pipeline infrastructure
 resource "google_storage_bucket" "default" {
   name          = var.iac_state_bucket_name
-  force_destroy = false
+  # For real workloads, set this to false to prevent destruction and retain version control history of state files
+  force_destroy = true
   location      = "US"
   storage_class = "STANDARD"
   versioning {
